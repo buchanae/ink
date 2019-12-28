3b32d417364d735031a80aa1f6173b01bb98278b
Alex Buchanan <buchanae@gmail.com>
2019-12-27 18:24:24 -0800

    tweak circle example

---
+++ examples/015_tweak.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/buchanae/ink/color"
+	"github.com/buchanae/ink/dd"
+	. "github.com/buchanae/ink/gfx"
+	"github.com/buchanae/ink/rand"
+)
+
+func Ink(doc *Doc) {
+	Clear(doc, color.White)
+
+	c := dd.Circle{
+		XY:       dd.XY{0.5, 0.5},
+		Radius:   0.3,
+		Segments: 40,
+	}
+	m := c.Mesh()
+	rand.TweakMesh(m, 0.03)
+
+	Fill{m, color.Red}.Draw(doc)
+}
