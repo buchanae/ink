d01e207868448666ecea0ee206f576a7dc84d768
Alex Buchanan <buchanae@gmail.com>
2020-02-09 13:25:21 -0800

    remove unused gfx.FillFrag

--- gfx/fill.go
+++ gfx/fill.go
@@ -25,14 +25,3 @@ func (f Fill) Shader() *Shader {
 func (f Fill) Draw(l Layer) {
 	f.Shader().Draw(l)
 }
-
-const FillFrag = `
-#version 330 core
-
-uniform vec4 u_color;
-out vec4 f_color;
-
-void main() {
-  f_color = u_color;
-}
-`
